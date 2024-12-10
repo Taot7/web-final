94d499e72b51b4dc7a474cc7bcce2ca4ab8241d1
jscomet <3059741813@qq.com>
2024-12-10 09:16:27 +0800

    fix: 个人中心获取我的待完成作业接口

--- src/services/api/assignment.ts
+++ src/services/api/assignment.ts
@@ -51,12 +51,72 @@ export async function getAssignments(
   });
 }
 
+/** 获取作业详情列表，携带提交情况 GET /assignment/list-detail */
+export async function getAssignmentDetails(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getAssignmentDetailsParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.ListResultAssignmentDetailVO>("/assignment/list-detail", {
+    method: "GET",
+    params: {
+      // current has a default value: 1
+      current: "1",
+      // pageSize has a default value: 10
+      pageSize: "10",
+      ...params,
+      param: undefined,
+      ...params["param"],
+    },
+    ...(options || {}),
+  });
+}
+
+/** 获取我的作业详情列表，携带提交情况 GET /assignment/list-detail-self */
+export async function getMyAssignmentDetails(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getMyAssignmentDetailsParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.ListResultAssignmentDetailVO>(
+    "/assignment/list-detail-self",
+    {
+      method: "GET",
+      params: {
+        // current has a default value: 1
+        current: "1",
+        // pageSize has a default value: 10
+        pageSize: "10",
+        ...params,
+        param: undefined,
+        ...params["param"],
+      },
+      ...(options || {}),
+    }
+  );
+}
+
 /** 发布作业,并为每个选课的学生生成作业提交记录 POST /assignment/publish */
 export async function publishAssignment(
   body: API.Assignment,
   options?: { [key: string]: any }
 ) {
   return request<boolean>("/assignment/publish", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 学生提交作业，如果没有提交记录则创建,有则更新 POST /assignment/submit */
+export async function submitAssignment(
+  body: API.AssignmentSubmitParam,
+  options?: { [key: string]: any }
+) {
+  return request<boolean>("/assignment/submit", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
